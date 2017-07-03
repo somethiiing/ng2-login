71dfc93b24259fae97c164baee793e72b8a5da91
Wilson Yu <hello@wilsonyu.io>
2017-07-03 19:54:52 -0400

    implement api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,13 +11,14 @@ import { routes } from './routes';
 @NgModule({
   declarations: [
     AppComponent,
-    Auth,
-    ActualStuff
+    ActualStuff,
+    Auth
   ],
   imports: [
     BrowserModule,
     RouterModule,
     FormsModule,
+    HttpModule,
     routes
   ],
   providers: [],
---
+++ src/app/services/api.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class ApiService {
+  headers: Headers = new Headers({
+    'Content-type': 'application/json',
+    Accept: 'application/json'
+  });
+
+  api_url = 'localhost:3000';
+
+  constructor(private http: Http) { }
+
+  getJson(resp) {
+    return resp.json();
+  }
+
+  checkForError(resp) {
+    if (resp.status >= 200 && resp.status < 300) {
+      return resp;
+    } else {
+      const error = new Error(resp.statusText);
+      error['response'] = resp;
+      console.error(error);
+      throw error;
+    }
+  }
+
+  post(path: string, data: any): Observable<any> {
+    return this.http.post(`${this.api_url}${path}`, JSON.stringify(data), {headers: this.headers})
+      .map(this.checkForError)
+      .catch(err => Observable.throw(err))
+      .map(this.getJson)
+  }
+
+
+
+}
+
